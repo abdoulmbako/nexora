d68a55007927103da8065721579d7ed6130f8d53
abdoulmbako <musaabdurrahman078@gmail.com>
2026-03-16 16:55:50 +0100

    Add user/vendor dashboard page

---
+++ Pages/dashboard.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">User / Vendor Dashboard</h1>
+    </div>
+  )
+}
